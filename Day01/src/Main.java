bbce125d240c470a859ad5add3d546749fac70d1
Denis Sept <de631sep@htwg-konstanz.de>
2023-12-02 21:46:52 +0100

    done hihi

--- Day01/src/Main.java
+++ Day01/src/Main.java
@@ -45,29 +45,29 @@ public class Main {
             wordToNumber.put("eight", "e8t");
             wordToNumber.put("nine", "n9e");
         }
-            try (BufferedReader reader = new BufferedReader(new FileReader("G:\\Meine Ablage\\Programmieren\\AOC_2023\\Day01\\Part1_Input.txt"))) {
-                String line;
-                int sum = 0;
-                String res;
-                while ((line = reader.readLine()) != null) {
-                    for (Map.Entry<String, String> entry : wordToNumber.entrySet()) {
-                        line = line.replace(entry.getKey(), entry.getValue());
-                    }
-                    StringBuilder sb = new StringBuilder();
-                    for (char c : line.toCharArray()) {
-                        if (Character.isDigit(c)) {
-                            sb.append(c);
-                        }
-                    }
-                    res = sb.toString();
-                    int number1 = res.charAt(0) - '0';
-                    int number2 = res.charAt(res.length() - 1) - '0';
-                    int end = number1 * 10 + number2;
-                    sum += end;
+        try (BufferedReader reader = new BufferedReader(new FileReader("G:\\Meine Ablage\\Programmieren\\AOC_2023\\Day01\\Part1_Input.txt"))) {
+            String line;
+            int sum = 0;
+            String res;
+            while ((line = reader.readLine()) != null) {
+                for (Map.Entry<String, String> entry : wordToNumber.entrySet()) {
+                    line = line.replace(entry.getKey(), entry.getValue());
                 }
-                System.out.printf("Sum: " + sum);
+                StringBuilder sb = new StringBuilder();
+                for (char c : line.toCharArray()) {
+                    if (Character.isDigit(c)) {
+                        sb.append(c);
+                    }
+                }
+                res = sb.toString();
+                int number1 = res.charAt(0) - '0';
+                int number2 = res.charAt(res.length() - 1) - '0';
+                int end = number1 * 10 + number2;
+                sum += end;
+            }
+            System.out.printf("Sum: " + sum);
             } catch (IOException e) {
                 e.printStackTrace();
-            }
         }
     }
+}
